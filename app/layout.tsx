46863539e9f90296c05792617ed39ec5326ff41d
Elvis Kerebi <kerebipreye@gmail.com>
2025-10-07 20:39:57 +0200

    added shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${inter.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
